ce5a4cbc17668bedd063c993b21cb42c19c447b5
Justin Wozniak <woz@anl.gov>
2025-07-08 12:35:48 -0400

    Check errors; add timing

--- workflow/step1/sub_p1_s1.sh
+++ workflow/step1/sub_p1_s1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH -A CHM155_001
+#SBATCH -A chm155_004
 #SBATCH -J p1_s1
 #SBATCH -o %x-%j.out
 #SBATCH -e %x-%j.err
-#SBATCH -t 1:00:00
+#SBATCH -t 20:00
 #SBATCH -p batch
 #SBATCH -q debug
 #SBATCH -N 1
@@ -19,9 +19,12 @@ export OE_LICENSE=/ccs/proj/chm155/IMPECCABLE/High-Throughput-Docking/oe_license
 source /ccs/proj/chm155/IMPECCABLE/activate_conda.sh
 conda activate oepython_new
 
+set -x
+set -eu
+
 # Setting paths
 CODE_DIR=/lustre/orion/chm155/proj-shared/apbhati/IMPECCABLE_2.0/htp_docking
-WORK_DIR=/lustre/orion/chm155/proj-shared/apbhati/IMPECCABLE_2.0/DEBUG_RUN/step1
+WORK_DIR=/lustre/orion/chm155/proj-shared/$USER/IMPECCABLE_2.0/workflow/step1
 MEM_ID=0
 MEM_DIR=$WORK_DIR/mem$MEM_ID
 mkdir -p $MEM_DIR
@@ -39,9 +42,28 @@ NNODES=1
 TASKS_PER_NODE=64
 
 # Executing runs
-srun -N ${NNODES} -n $((NNODES*TASKS_PER_NODE)) -S 0 --ntasks-per-node=64 python $WORK_DIR/docking_openeye.py
+SRUN_ARGS=(
+  -N ${NNODES}
+  -n $((NNODES*TASKS_PER_NODE))
+  -S 0
+  --ntasks-per-node=64
+)
+
+(
+  set -x
+  pwd -P
+  ls
+  /usr/bin/time --format="TIME: %E" \
+        srun ${SRUN_ARGS[@]} python $WORK_DIR/docking_openeye.py
+)
 
 # Validating runs
-cd $MEM_DIR
-python $WORK_DIR/validate_1.py -s ./scores -c config_htp.json
+(
+  set -x
 
+  cd $MEM_DIR
+  pwd -P
+  ls
+  /usr/bin/time --format="TIME: %E" \
+                python $WORK_DIR/validate_1.py -s ./scores -c config_htp.json
+)
