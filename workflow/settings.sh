bfdc210c1ada1d7c7a7fd33db51c0022ef3369fe
Justin Wozniak <woz@anl.gov>
2025-08-21 11:43:33 -0500

    Fix settings for wozniak

--- workflow/settings.sh
+++ workflow/settings.sh
@@ -1,10 +1,12 @@
 
-# Workflow settings- exported to M4 filter
+# Per-user workflow settings- exported to M4 filter
+# Do not commit user changes to git!
 
 # Location under WORK_DIR
 export SMILES_INPUT=input/Training.smi
 # Location under WORK_DIR
 export RECEP_FILE=input/rec_4ui5.oedu
-export INFERENCE_DATA=/lus/flare/projects/workflow_scaling/wozniak/IMPECCABLE/InferenceData
+# export INFERENCE_DATA=/lustre/orion/chm155/proj-shared/wozniak/InferenceData
+export INFERENCE_DATA=/lustre/orion/chm155/proj-shared/avasan/InferenceData
 export DATASET=BDB
 export SAMPLE_SIZE=1000
