b1204ac59fe46558632c019cff9f07f6b81360ed
Justin Wozniak <woz@anl.gov>
2025-08-22 14:15:30 -0500

    Better comment

--- workflow/settings.sh
+++ workflow/settings.sh
@@ -2,9 +2,9 @@
 # Per-user workflow settings- exported to M4 filter
 # Do not commit user changes to git!
 
-# Location under WORK_DIR
+# Location relative to WORK_DIR
 export SMILES_INPUT=input/Training.smi
-# Location under WORK_DIR
+# Location relative to WORK_DIR
 export RECEP_FILE=input/rec_4ui5.oedu
 # export INFERENCE_DATA=/lustre/orion/chm155/proj-shared/wozniak/InferenceData
 export INFERENCE_DATA=/lustre/orion/chm155/proj-shared/avasan/InferenceData
