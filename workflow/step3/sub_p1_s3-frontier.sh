096f27f01122b122448ecaf4260728419422b652
Justin Wozniak <woz@anl.gov>
2025-08-25 10:55:49 -0500

    Step 3 works on Frontier

---
+++ workflow/step3/sub_p1_s3-frontier.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#SBATCH -A CHM155_004
+#SBATCH -J p1_s3
+# Merge streams:
+#SBATCH -o %x-%j.out
+#SBATCH -e %x-%j.out
+#SBATCH -t 1:00:00
+#SBATCH -p batch
+#SBATCH -q debug
+#SBATCH -N 1
+#SBATCH --tasks-per-node 8
+#SBATCH -S 0
+
+export THIS=$SLURM_SUBMIT_DIR
+cd $THIS
+
+export SITE=frontier
+
+source $THIS/../impeccable-settings.sh
+source $THIS/sub_p1_s3-setup.sh st_mpi_base
+
+
+# Setting environments
+module reset
+module load PrgEnv-gnu
+module load rocm/6.0.0
+
+export TF_FORCE_GPU_ALLOW_GROWTH=true
+export MIOPEN_USER_DB_PATH=/tmp/$USER/miopen-cache
+export MIOPEN_CUSTOM_CACHE_DIR=$MIOPEN_USER_DB_PATH
+if [[ -d $MIOPEN_USER_DB_PATH ]]
+then
+  rm -r  $MIOPEN_USER_DB_PATH
+fi
+mkdir -p $MIOPEN_USER_DB_PATH
+
+set -eu
+set -x
+
+# Executing runs
+srun -N ${NNODES} -n $((NNODES*TASKS_PER_NODE)) -S 0 python3 $WORK_DIR/smiles_regress_transformer_run_large.py -w $STEP2_DIR/model.weights.h5 -d $DATASET
+
+# post-processing
+cd $MEM_DIR
+python3 $WORK_DIR/sorting.py
+
+# Validating runs
+cd $MEM_DIR
+python $WORK_DIR/validate_3.py
+
---
+++ workflow/step3/sub_p1_s3-setup.sh
@@ -0,0 +1,32 @@
+
+# SUB P1 S3 SETUP
+# Portable setup script
+# Assumes SITE is in the environment
+
+CONDA_ENVIRONMENT=$1
+
+THIS=$( realpath $( dirname $0 ) )
+source $THIS/../site-${SITE:-UNKNOWN}-settings.sh $CONDA_ENVIRONMENT
+
+CODE_DIR=$IMPECCABLE_CODE/surrogate_inference
+export WORK_DIR=$WORK_TOP/step3
+
+MEM_ID=0
+MEM_DIR=$WORK_DIR/mem$MEM_ID
+mkdir -p $MEM_DIR
+STEP2_DIR=$WORK_DIR/../step2/mem$MEM_ID
+
+# Setting runs
+cp -r $CODE_DIR/* $WORK_DIR/
+cd $MEM_DIR
+DATASET=BDB
+mkdir -p output/$DATASET
+mkdir -p Sorting_all
+#rm -f model.weights.h5
+cp $WORK_DIR/config_inference.json $MEM_DIR
+sed -i "s/ZIN/$DATASET/g" config_inference.json
+sed -i "s@PLACEHOLDER_DIR/VocabFiles@${WORK_DIR}/VocabFiles@g" config_inference.json
+sed -i "s/ 400,/ 5,/g" config_inference.json
+NNODES=1
+TASKS_PER_NODE=8
+set +eux
