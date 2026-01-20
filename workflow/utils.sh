b0139b5cff89cdd13372401b113f97b607a0167b
Justin Wozniak <woz@anl.gov>
2026-01-20 12:13:29 -0600

    New report_conda()

--- workflow/utils.sh
+++ workflow/utils.sh
@@ -17,3 +17,20 @@ bak()
     mv -v --backup=numbered --no-target-directory $FILE $FILE.bak
   fi
 }
+
+report_conda()
+# Assumes LABEL is a global
+{
+  if ! which python
+  then
+    echo "$LABEL: no python in PATH!"
+    return 1
+  fi
+  if [[ ${CONDA_PREFIX:-0} == 0 ]]
+  then
+    echo "$LABEL: no CONDA_PREFIX: set up Anaconda"
+    return 1
+  fi
+  echo "$LABEL: python:      " $( which python )
+  echo "$LABEL: CONDA_PREFIX:" ${CONDA_PREFIX}
+}
