396ebfdaa579e459d22b132b3139f184da5ead16
Justin Wozniak <woz@anl.gov>
2026-02-06 15:43:10 -0600

    More software to install

--- workflow/install-py-pkgs.sh
+++ workflow/install-py-pkgs.sh
@@ -4,11 +4,15 @@
 # User can tar this Anaconda installation for faster reuse
 #   See python-aurora.sh
 
+# Install OpenEye by copying from Frontier
+
 CHANNELS=( -c conda-forge
            -c openeye
          )
 PKGS=( pydantic-settings
        openeye-toolkits
+       # Step 4a:
+       mdanalysis
      )
 
 conda install ${CHANNELS[@]} ${PKGS[@]}
