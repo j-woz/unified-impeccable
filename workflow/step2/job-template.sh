604cdcd698efc2305767762c0a704c6ebc08f822
Justin Wozniak <woz@anl.gov>
2026-04-30 15:30:03 -0500

    New job-template

---
+++ workflow/step2/job-template.sh
@@ -0,0 +1,13 @@
+
+# STEP 2 JOB SETTINGS
+
+export NAME=p1_s2
+export WALLTIME=00:20:00
+export QUEUE=debug-scaling
+export NODES=1
+export PPN=1
+
+# Location for job scripts and stdout/stderr logs
+export IMPECCABLE_WORK=/lus/flare/projects/workflow_scaling/wozniak/IMPECCABLE/work/jobs
+
+
