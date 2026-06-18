c7ae45ebcd17ef55048846652573bd51a0b921b4
Justin Wozniak <woz@anl.gov>
2026-06-18 11:39:40 -0500

    Fixes to S4C

--- workflow/step4/sub_p2_s4c-aurora.sh
+++ workflow/step4/sub_p2_s4c-aurora.sh
@@ -2,47 +2,45 @@
 
 # SUB P2 S4C AURORA
 
-#PBS -A workflow_scaling
-#PBS -N p2_s4c
+#PBS -A IMPECCAFLOW
+#PBS -N m4_getenv(NAME)
 # Merge streams:
-#PBS -o sub_p2_s4c.out
+#PBS -o m4_getenv(OUTPUT)
 #PBS -j oe
-#PBS -l walltime=0:05:00
-#PBS -q debug
-#PBS -l nodes=1:ppn=64
+#PBS -l walltime=m4_getenv(WALLTIME)
+#PBS -q m4_getenv(QUEUE)
+#PBS -l nodes=m4_getenv(NODES):ppn=64
 #PBS -l filesystems=home:flare
-
-# #SBATCH -A CHM155_001
-# #SBATCH -J p2_s4c
-# #SBATCH -o %x-%j.out
-# #SBATCH -e %x-%j.err
-# #SBATCH -t 1:00:00
-# #SBATCH -p batch
-# ##SBATCH -q debug
-# #SBATCH -N 1
-# ##SBATCH --tasks-per-node 64
-# #SBATCH -S 0
 
 set -eu
 
-THIS=$( realpath $( dirname $0 ) )
-cd $THIS
+SITE=aurora
+LABEL=m4_getenv(NAME)
+NODES=m4_getenv(NODES)
+PPN=m4_getenv(PPN)
 
-export SITE=aurora
+WORKFLOW_STEP=m4_getenv(WORKFLOW_STEP)
+cd $WORKFLOW_STEP
 
-source $THIS/../impeccable-settings.sh
-source $THIS/sub_p2_s4c-setup.sh
+echo "JOB:" $PBS_JOBID
+
+source $WORKFLOW_STEP/../utils.sh
+
+SETTINGS_IMPECCABLE=m4_getenv(SETTINGS_IMPECCABLE)
+source-checked $SETTINGS_IMPECCABLE
+
+source $WORKFLOW_STEP/sub_p2_s4c-setup.sh
 
 # export NAMDBINDIR=/lustre/orion/stf006/world-shared/dilipa/NAMD/NAMD31A3
 # export NAMDVER=NAMD_3.1alpha3_Linux-x86_64-multicore-HIP
 # export NAMDBIN=$NAMDBINDIR/$NAMDVER/namd3
 
-NAMDBIN=$HOME/proj/uni-impec/namd3_md_example/namd3
+NAMDBIN=$HOME/proj/uni-impecc/namd3_md_example/namd3
 export namdargs=''
 
 export ZE_FLAT_DEVICE_HIERARCHY=FLAT
 
-LABEL="S4C"
+START=
 
 # Executing runs
 for ((i=0; i < $N_COMPS; i++))
@@ -56,6 +54,6 @@ do
   #sed -i "s/dcdfreq             200/dcdfreq             100/" eq0.conf
   #srun -N1 -n1 -c56 $NAMDBIN --tclmain eq0.conf +stdout eq0.log
   # srun -N1 -n1 -c56 --ntasks-per-node=1 --gpus-per-task=1 --gpu-bind=closest --mpi=none --kill-on-bad-exit=0
-  tm $NAMDBIN +p56 +devices 0 $namdargs --tclmain eq0.conf +stdout eq0.log
+  $NAMDBIN +p56 +devices 0 $namdargs --tclmain eq0.conf +stdout eq0.log
   cd ../..
 done
