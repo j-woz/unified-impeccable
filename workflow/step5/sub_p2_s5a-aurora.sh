6d0ef34e8631475b15c9d7c68fb51d9823428911
Justin Wozniak <woz@anl.gov>
2026-06-18 11:39:26 -0500

    More S5

--- workflow/step5/sub_p2_s5a-aurora.sh
+++ workflow/step5/sub_p2_s5a-aurora.sh
@@ -2,38 +2,27 @@
 
 # SUB P2 S5A AURORA
 
-#PBS -A workflow_scaling
-#PBS -N p2_s4c
+#PBS -A IMPECCAFLOW
+#PBS -N m4_getenv(NAME)
 # Merge streams:
-#PBS -o sub_p2_s4c.out
+#PBS -o m4_getenv(OUTPUT)
 #PBS -j oe
-#PBS -l walltime=0:05:00
-#PBS -q debug
-#PBS -l nodes=1:ppn=64
+#PBS -l walltime=m4_getenv(WALLTIME)
+#PBS -q m4_getenv(QUEUE)
+#PBS -l nodes=m4_getenv(NODES):ppn=64
 #PBS -l filesystems=home:flare
-
-# #SBATCH -A CHM155_001
-# #SBATCH -J p2_s5a
-# #SBATCH -o %x-%j.out
-# #SBATCH -e %x-%j.err
-# #SBATCH -t 1:00:00
-# #SBATCH -p batch
-# ##SBATCH -q debug
-# #SBATCH -N 1
-# #SBATCH --tasks-per-node 64
-# #SBATCH -S 0
 
 set -eu
 
-THIS=$( realpath $( dirname $0 ) )
-cd $THIS
-
 export SITE=aurora
+LABEL=m4_getenv(NAME)
+NODES=m4_getenv(NODES)
+PPN=m4_getenv(PPN)
 
-source $THIS/../impeccable-settings.sh
-source $THIS/sub_p2_s5a-setup.sh \
-       /opt/aurora/24.180.3/oneapi/intel-conda-miniforge \
-       /tmp/PY-IMPECCABLE/steps123
+source $WORKFLOW_STEP/../impeccable-settings.sh
+source $WORKFLOW_STEP/sub_p2_s5a-setup.sh \
+       /opt/aurora/25.190.0/oneapi/intel-conda-miniforge \
+       /tmp/PY-IMPECCABLE/step4
 
 # Executing runs
 cd $ITR_DIR
